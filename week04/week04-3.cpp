4fb2eff3626dbeb20dd1025ed8f0a97ba1ec6ce0
Chhokmah00 <5m4wu6@gmail.com>
2024-03-15 10:38:22 +0800

    week04

---
+++ week04/week04-3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=9;i++){
+        printf("i¬O%d\n",i);
+    }
+    printf("\n");
+    for(int i=9;i>=1;i--){
+        printf("i¬O%d\n",i);
+  }
+}
+
+

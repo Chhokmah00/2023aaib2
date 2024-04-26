d7a532a1280bd9b82ac81f42a2237ef24002192e
Chhokmah00 <5m4wu6@gmail.com>
2024-04-26 10:44:21 +0800

    week10

---
+++ week10/week10-2.cpp
@@ -0,0 +1,14 @@
+///輾轉相除法，a大的，b中的，c小的a%b
+#include <stdio.h>
+int gcd(int a,int b){
+    if(a==0) return b;
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+int main()
+{
+    printf("請輸入a b兩個數字: ");
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("最大公因數是: %d\n",gcd(a,b));
+}

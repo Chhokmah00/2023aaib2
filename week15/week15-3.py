a20a79fc625ee4747061d45ec3fafb15d0c37336
Chhokmah00 <145088569+Chhokmah00@users.noreply.github.com>
2024-05-31 11:41:39 +0800

    Delete week15 directory
    Delete week15 directory

--- week15/week15-3.py
+++
@@ -1,8 +0,0 @@
-#寫到一半的
-class Solution:
-    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        N=len(s)
-        for i in range(N): #先用Python把字串的迴圈寫出來
-            print(s[i],t[i])
-        ans=0
-        return ans
